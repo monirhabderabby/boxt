df53537b36e9c8bd08c71abe9f3c5481d783b59a
unknown <monirhrabby.personal@gmail.com>
2025-12-28 15:41:51 +0600

    developed: product section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./_components/banner";
 import OurPartners from "./_components/our-partner";
+import Product from "./_components/product";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-10">
       <Banner />
       <OurPartners />
+      <Product />
     </div>
   );
 }
